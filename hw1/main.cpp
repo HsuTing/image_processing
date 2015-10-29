f9ffc2a03ea26e027c556f4367adf3f65bbd9831
hsuting <hsuting0106@gmail.com>
2015-10-29 13:19:06 +0800

    modify main.cpp to use all part

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -9,13 +9,12 @@
 using namespace std;
 
 int main() {
-  /*ReadImage part_one_one("data/test.jpg");
+  ReadImage part_one_one("data/test.jpg");
   DrawFrame part_one_two(part_one_one.GetMat());
   Drawing part_one_three(part_one_two.GetMat(), part_one_two.GetStart(), part_one_two.GetEnd());
-  */
 
-  //ReadVideo part_two_one("data/test.mp4");
-  //ShowVideoTime part_two_two("data/test.mp4");
+  ReadVideo part_two_one("data/test.mp4");
+  ShowVideoTime part_two_two("data/test.mp4");
   VideoSlideBar part_two_three("data/test.mp4");
 
   return 0;
